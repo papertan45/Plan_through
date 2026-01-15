9b8259438154327749cf6f9a38d095ddd0a0eab1
AC7014 <89561394+noname33245@users.noreply.github.com>
2026-01-15 23:19:29 +0800

    Add system tray functionality and related settings

--- mainwindow.h
+++ mainwindow.h
@@ -24,8 +24,33 @@
 #include <QDir>
 #include <QProcessEnvironment>
 #include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QDesktopServices>
+#include <QUrl>
+#include <QSettings>
+#include <QColor>
+#include <QPalette>
+#include <QCloseEvent>
+#include <QGuiApplication>
+#include <QScreen>
 
-#include "appdatas.h"
+struct TimeAxisItem
+{
+    QString type;
+    bool isCompleted;
+};
+
+struct DateStudyData
+{
+    int studyHours = 0;
+    int completedProjects = 0;
+    int totalProjects = 0;
+    QMap<int, TimeAxisItem> timeAxisData;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -34,6 +59,10 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void initSettings();
+    void saveSettings();
+    void applyTheme(int themeType);  
+    void setAutoStartup(bool isAuto);
 
 private slots:
     void switchToDayView();
@@ -48,8 +77,25 @@ private slots:
     void setToCurrentMonth();
     void showSetTargetDialog();
     void setStudyTargetHour(int targetHour);
+    void showSettingsWindow();
+    void onTrayIconClicked(QSystemTrayIcon::ActivationReason reason);
+    void showWindowFromTray();
+    void onAutoStartupChanged(int state);
+    void onMinToTrayChanged(int state);
+    void onThemeChanged(int index); 
+    void openSavePath();
+    void goToMsStoreRate();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
 
 private:
+    void initSavePath();
+    void saveDataToFile();
+    void loadDataFromFile();
+    void initConfigFile();
+    void saveConfigToFile();
+    void loadConfigFromFile();
     void initUI();
     QWidget* createDayViewPage();
     QWidget* createMonthViewPage();
@@ -58,9 +104,11 @@ private:
     void updateDayViewStats();
     void generateMonthCalendar(int year, int month);
     int calculateContinuousDays();
+    void initSystemTray();
 
     QPushButton *m_dayViewBtn = nullptr;
     QPushButton *m_monthViewBtn = nullptr;
+    QPushButton *m_settingsBtn = nullptr;
     QStackedWidget *m_mainStackedWidget = nullptr;
     QLabel *m_todayStudyHourLabel = nullptr;
     QProgressBar *m_dayProgressBar = nullptr;
@@ -78,6 +126,19 @@ private:
     QDate m_currentDate;
     int m_currentYear;
     int m_currentMonth;
+    QString m_saveFilePath;
+    QString m_configFilePath;
+    QMap<QDate, DateStudyData> m_studyDataMap;
+    int m_maxContinuousDays = 0;
+    int m_studyTargetHour = 4;
+
+    QSystemTrayIcon *m_systemTrayIcon = nullptr;
+    QMenu *m_trayMenu = nullptr;
+    bool m_isMinToTray = false;
+    bool m_isAutoStartup = false;
+    int m_themeType = 0; 
+    QSettings *m_appSettings = nullptr;
+    QString m_appDataPath;
 };
 
-#endif // MAINWINDOW_H
+#endif
