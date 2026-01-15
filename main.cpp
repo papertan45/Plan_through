526ae5511bd0904d4949b16d7395f00815e23601
AC7014 <89561394+noname33245@users.noreply.github.com>
2026-01-15 23:18:42 +0800

    Add QApplication attribute for native widgets

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
